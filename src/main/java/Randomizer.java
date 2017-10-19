d459919caef7407ea9656a6e1aa5253febb1dae8
rulemur <rulemur@gmail.com>
2017-10-19 21:11:00 +0300

    fu

--- src/main/java/Randomizer.java
+++ src/main/java/Randomizer.java
@@ -1,3 +1,4 @@
+import java.text.DecimalFormat;
 import java.util.Random;
 
 public class Randomizer {
@@ -6,5 +7,14 @@ public class Randomizer {
         Random rnd = new Random();
 
         return rnd.nextInt(to - from) + from;
+    }
+
+
+    public static double getRandomDouble(double from, double to) {
+        Random rnd = new Random();
+
+        double number = rnd.nextDouble() * (to - from) + from;
+//        number = (double) Math.round(number * 100d) / 100d;
+        return number;
     }
 }
